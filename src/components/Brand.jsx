090dd4dc25ec7d52b5c9082e48647a01557d367a
lbrnryn <lbrnryn@yahoo.com>
2023-04-15 22:07:18 +0800

    Modified the brand component

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -5,60 +5,55 @@ import { BsPencilFill, BsFillTrashFill } from "react-icons/bs";
 
 function Brand({ brands, setBrands }) {
 
-    const [brandID, setBrandID] = useState("");
-    const [brandName, setBrandName] = useState("");
-    const [edit, setEdit] = useState(false);
+    const [brandID, setBrandID] = useState('')
+    const [brandName, setBrandName] = useState('')
+    const [editBrand, setEditBrand] = useState(false)
 
-    const addBrand = (e) => {
+    const handleBrandName = (e) => { setBrandName(e.target.value) }
+
+    const handleAddBrand = (e) => {
         e.preventDefault();
-        setBrands([...brands, { _id: uuidv4(), name: brandName }]);
-        setBrandName("");
+
+        setBrands([...brands, { _id: uuidv4(), name: brandName }])
+        setBrandName('')
     }
 
-    const editBrand = ({ _id, name}) => {
-        setEdit(true);
-        setBrandName(name);
-        setBrandID(_id);
-    };
+    const handleEditBrand = ({ _id, name }) => {
+        setBrandName(name)
+        setBrandID(_id)
+        setEditBrand(!editBrand)
+    }
 
-    const updateBrands = (e) => {
+    const handleUpdateBrand = (e) => {
         e.preventDefault();
         
-        const copyBrands = [...brands];
-        const foundBrand = copyBrands.find(brand => brand._id === brandID);
-        foundBrand.name = brandName;
+        const updBrands = brands.map(brand => brand._id !== brandID ? brand : { ...brand, name: brandName })
 
-        setBrands(copyBrands);
-        setBrandID("");
-        setBrandName("");
-    };
+        setBrands(updBrands)
+        handleCancelEditBrand()
+    }
 
-    const cancelEditSubmit = () => {
-        setEdit(false);
-        setBrandID("");
-        setBrandName("");
-    };
+    const handleCancelEditBrand = () => {
+        setBrandName('')
+        setBrandID('')
+        setEditBrand(!editBrand)
+    }
 
-    const deleteBrand = ({ _id }) => {
-        if (confirm(`Are you sure you want to delete this brand?`)) {
-            const updBrands = brands.filter(brand => brand._id !== _id);
-            setBrands(updBrands);
-            setEdit(false);
-            setBrandID("");
-            setBrandName("");
-        }
-    };
+    const handleDeleteBrand = (id) => { 
+        setBrands(brands.filter(brand => brand._id !== id))
+        handleCancelEditBrand() 
+    }
 
     return (
         <>
-            <Form className="d-grid gap-2 mb-3" onSubmit={ e => edit ? updateBrands(e) : addBrand(e) }> 
+            <Form className="d-grid gap-2 mb-3" onSubmit={ !editBrand ? handleAddBrand : handleUpdateBrand }> 
 
-                <Form.Control type="text" size="sm" className="bg-transparent text-white" onChange={ e => setBrandName(e.target.value) } value={ brandName } placeholder="Brandname..."/>
+                <Form.Control type="text" size="sm" className="bg-transparent text-white" placeholder="Brandname..." value={ brandName } onChange={ handleBrandName } />
 
                 <div className="d-flex justify-content-between align-items-center">
                     <div>
-                        <Button type="submit" variant="primary" className="badge">{!edit ? "Submit": "Edit"}</Button>
-                        <Button type="button" variant="secondary" className={ `badge ${!edit && "d-none"}` } onClick={ cancelEditSubmit }>Cancel</Button>
+                        <Button type="submit" variant="primary" className="badge">{ !editBrand ? "Submit" : "Update" }</Button>
+                        <Button type="button" variant="secondary" className={ `badge ${!editBrand && "d-none"}` } onClick={ handleCancelEditBrand }>Cancel</Button>
                     </div>
                     <span className="text-white small">{ brands.length }</span>
                 </div>
@@ -69,12 +64,12 @@ function Brand({ brands, setBrands }) {
 
                 { brands.map(brand => (
                     <ListGroup.Item className="py-1 px-2 primary-bg-color text-white border-0 d-flex justify-content-between align-items-center" key={ brand._id }>
-                        <small className="text-capitalize">{ brand.name }</small>
+                        <small>{brand.name}</small>
                         <div>
-                            <Button type="button" variant="" className="badge" onClick={ () => editBrand(brand) }>
+                            <Button type="button" variant="" className="badge" onClick={ () => handleEditBrand(brand) }>
                                 <BsPencilFill className="text-primary" /> 
                             </Button>
-                            <Button type="button" variant="" className="badge" onClick={ () => deleteBrand(brand) }>
+                            <Button type="button" variant="" className="badge" onClick={ () => handleDeleteBrand(brand._id) }>
                                 <BsFillTrashFill className="text-danger" />
                             </Button>
                         </div>
@@ -84,6 +79,88 @@ function Brand({ brands, setBrands }) {
             </ListGroup>
         </>
     )
+
 }
 
 export default Brand;
+
+
+    // const [brandID, setBrandID] = useState("");
+    // const [brandName, setBrandName] = useState("");
+    // const [edit, setEdit] = useState(false);
+
+    // const handleAddBrand = (e) => {
+    //     e.preventDefault();
+    //     setBrands([...brands, { _id: uuidv4(), name: brandName }]);
+    //     setBrandName("");
+    // }
+
+    // const handleEditBrand = ({ _id, name}) => {
+    //     setEdit(true);
+    //     setBrandName(name);
+    //     setBrandID(_id);
+    // };
+
+    // const handleUpdateBrands = (e) => {
+    //     e.preventDefault();
+        
+    //     const copyBrands = [...brands];
+    //     const foundBrand = copyBrands.find(brand => brand._id === brandID);
+    //     foundBrand.name = brandName;
+
+    //     setBrands(copyBrands);
+    //     setBrandID("");
+    //     setBrandName("");
+    // };
+
+    // const handleCancelEditSubmit = () => {
+    //     setEdit(false);
+    //     setBrandID("");
+    //     setBrandName("");
+    // };
+
+    // const handleDeleteBrand = ({ _id }) => {
+    //     if (confirm(`Are you sure you want to delete this brand?`)) {
+    //         const updBrands = brands.filter(brand => brand._id !== _id);
+    //         setBrands(updBrands);
+    //         setEdit(false);
+    //         setBrandID("");
+    //         setBrandName("");
+    //     }
+    // };
+
+    // return (
+    //     <>
+    //         <Form className="d-grid gap-2 mb-3" onSubmit={ e => edit ? handleUpdateBrands(e) : handleAddBrand(e) }> 
+
+    //             <Form.Control type="text" size="sm" className="bg-transparent text-white" onChange={ e => setBrandName(e.target.value) } value={ brandName } placeholder="Brandname..."/>
+
+    //             <div className="d-flex justify-content-between align-items-center">
+    //                 <div>
+    //                     <Button type="submit" variant="primary" className="badge">{!edit ? "Submit": "Edit"}</Button>
+    //                     <Button type="button" variant="secondary" className={ `badge ${!edit && "d-none"}` } onClick={ handleCancelEditSubmit }>Cancel</Button>
+    //                 </div>
+    //                 <span className="text-white small">{ brands.length }</span>
+    //             </div>
+
+    //         </Form>
+
+    //         <ListGroup className="gap-1">
+
+    //             { brands.map(brand => (
+    //                 <ListGroup.Item className="py-1 px-2 primary-bg-color text-white border-0 d-flex justify-content-between align-items-center" key={ brand._id }>
+    //                     <small className="text-capitalize">{ brand.name }</small>
+    //                     <div>
+    //                         <Button type="button" variant="" className="badge" onClick={ () => handleEditBrand(brand) }>
+    //                             <BsPencilFill className="text-primary" /> 
+    //                         </Button>
+    //                         <Button type="button" variant="" className="badge" onClick={ () => handleDeleteBrand(brand) }>
+    //                             <BsFillTrashFill className="text-danger" />
+    //                         </Button>
+    //                     </div>
+    //                 </ListGroup.Item>
+    //             )) }
+
+    //         </ListGroup>
+    //     </>
+    // )
